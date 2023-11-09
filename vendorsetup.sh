2df9b62a67a1bf96c6300d95e8db58f4693071b7
userariii <abhattacharjee717@gmail.com>
2023-11-09 13:51:57 +0000

    PL2: Import vendorsetup.sh
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Device
+git clone -b thirteen --depth=1 https://github.com/Nokia-SDM660-Devices/android_device_nokia_sdm660-common.git device/nokia/sdm660-common
+# Vendor
+git clone -b thirteen --depth=1 https://github.com/Nokia-SDM660-Devices/android_vendor_nokia_PL2.git vendor/nokia/PL2
+git clone -b thirteen --depth=1 https://github.com/Nokia-SDM660-Devices/android_vendor_nokia_sdm660-common vendor/nokia/sdm660-common
+# Kernel
+git clone -b thirteen-KSU --depth=1 https://github.com/Nokia-SDM660-Devices/android_kernel_nokia_sdm660.git kernel/nokia/sdm660
+# Dolby
+git clone -b main --depth=1 https://github.com/userariii/hardware_dolby.git hardware/dolby
+# Camera
+git clone -b thirteen --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_nokia_Camera.git vendor/nokia/Camera
